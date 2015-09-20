4b7695cd2869f7f205303d9a70854043d71fa859
luckydrq <drqzju@gmail.com>
2015-09-20 18:44:52 +0800

    feat: support multiple rows update

--- test/operator.test.js
+++ test/operator.test.js
@@ -15,9 +15,18 @@
  */
 
 const assert = require('assert');
+const rds = require('..');
+const config = require('./config');
 const Operator = require('../lib/operator');
 
 describe('operator.test.js', function () {
+  const prefix = 'prefix-' + process.version + '-';
+  const table = 'user';
+  before(function* () {
+    this.db = rds(config);
+    yield this.db.query('delete from ?? where name like ?', [table, prefix + '%']);
+  });
+
   describe('_where(where)', function () {
     it('should get where sql', function () {
       let op = new Operator();
@@ -50,6 +59,35 @@ describe('operator.test.js', function () {
       } catch (err) {
         assert.equal(err.message.indexOf('SubClass must impl this'), 0);
       }
+    });
+  });
+
+  describe('updateRows()', function() {
+    before(function* () {
+      yield this.db.query('insert into ??(name, email, gmt_create, gmt_modified) \
+        values(?, ?, now(), now())',
+        [table, prefix + 'luckydrq', prefix + 'drqzju@gmail.com']);
+      yield this.db.query('insert into ??(name, email, gmt_create, gmt_modified) \
+        values(?, ?, now(), now())',
+        [table, prefix + 'luckydrq2', prefix + 'drqzju@gmail.com']);
+    });
+
+    it('should update multiple rows', function* () {
+      let rows = yield this.db.query('select * from ?? where email=? order by id',
+        [table, prefix + 'drqzju@gmail.com']);
+      console.log(rows);
+      assert.equal(rows.length, 2);
+      assert.equal(rows[0].name, prefix + 'luckydrq');
+      assert.equal(rows[1].name, prefix + 'luckydrq2');
+
+      rows[0].name = prefix + 'drq';
+      rows[1].email = prefix + 'drqzju@gmail2.com';
+      yield this.db.updateRows(table, rows);
+      let updatedRows = yield this.db.query('select * from ?? where id in (?, ?)',
+        [table, rows[0].id, rows[1].id]);
+        console.log(updatedRows);
+      assert.equal(updatedRows[0].name, prefix + 'drq');
+      assert.equal(updatedRows[1].email, prefix + 'drqzju@gmail2.com');
     });
   });
 });
