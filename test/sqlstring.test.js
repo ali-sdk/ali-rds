412b959e7e04ae39aa91b27334fe89ea911d846e
Yiyu He <dead_horse@qq.com>
2017-09-25 22:32:53 -0500

    fix: don't redefined sqlstring.escape (#40)

--- test/sqlstring.test.js
+++ test/sqlstring.test.js
@@ -17,5 +17,14 @@ describe('sqlstring.test.js', function () {
       assert.equal(SqlString.escape(literals.Literal('sum2()')), 'sum2()');
       assert.equal(SqlString.escape(literals.now), mysql.escape(literals.now));
     });
+
+    it('should only patch once', function() {
+      require.cache[require.resolve('../lib/sqlstring')] = null;
+      const AnotherSqlString = require('../lib/sqlstring');
+      assert.equal(AnotherSqlString.escape(literals.now), 'now()');
+      assert.equal(AnotherSqlString.escape(new literals.Literal('sum')), 'sum');
+      assert.equal(AnotherSqlString.escape(literals.Literal('sum2()')), 'sum2()');
+      assert.equal(AnotherSqlString.escape(literals.now), mysql.escape(literals.now));
+    });
   });
 });
