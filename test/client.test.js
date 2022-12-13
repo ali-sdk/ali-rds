8c8c8e7365d04a91a24559a4a786c59bb4d98ce6
fengmk2 <fengmk2@gmail.com>
2022-12-14 00:49:38 +0800

    f

--- test/client.test.js
+++ test/client.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert/strict');
+const assert = require('assert').strict;
 const fs = require('fs/promises');
 const path = require('path');
 const RDSClient = require('..');
