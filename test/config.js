29e0cd09768cd7c8621e2db50cf42c1550cabf97
fengmk2 <fengmk2@gmail.com>
2022-12-14 00:47:04 +0800

    f

--- test/config.js
+++ test/config.js
@@ -1,9 +1,11 @@
+const database = process.env.CI ? 'unittest' : 'test';
+
 module.exports = {
   host: 'localhost',
   port: 3306,
   user: 'root',
   password: '',
-  database: process.env.MYSQL_DATABASE || 'test',
+  database,
   // host: env.ALI_SDK_RDS_HOST || 'localhost',
   // port: env.ALI_SDK_RDS_PORT || 3306,
   // user: env.ALI_SDK_RDS_USER || 'root',
