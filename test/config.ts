43a8275fadbb504aadd6ad1ffb6034c600b2fe0c
fengmk2 <fengmk2@gmail.com>
2023-03-04 23:50:21 +0800

    f

--- test/config.ts
+++ test/config.ts
@@ -1,11 +1,9 @@
-const database = process.env.CI ? 'unittest' : 'test';
-
 export default {
   host: 'localhost',
   port: 3306,
   user: 'root',
   password: '',
-  database,
+  database: 'test',
   // host: env.ALI_SDK_RDS_HOST || 'localhost',
   // port: env.ALI_SDK_RDS_PORT || 3306,
   // user: env.ALI_SDK_RDS_USER || 'root',
